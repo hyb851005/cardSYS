5f6b7a30549571631afa391730c7eb0dbe11462e
MSF2865963568 <2865963568@qq.com>
2019-01-11 20:00:02 +0800

    看

--- src/cardSYS/Application.java
+++ src/cardSYS/Application.java
@@ -7,7 +7,7 @@ import view.AppView;
 public class Application {
 
 	public static void main(String[] args) throws Exception {
-		// TODO Auto-generated method stub哈哈哈哈哈
+		// TODO Auto-generated method stub哈哈哈哈哈罗罗罗
 		
 		try {
 			AppView av = new AppView();
